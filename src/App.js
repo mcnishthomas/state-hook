f42e0f083fd1a8f233d99c4f076857e621917556
Thomas McNish <mcnishthomas@gmail.com>
2022-05-20 08:09:36 -0400

    added increase score button using hook

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Welcome!</h1>
+        <h1>{score}</h1>
+        <button onClick={() => setScore(score + 1)} >Increase Score </button>
       </header>
     </div>
   );
