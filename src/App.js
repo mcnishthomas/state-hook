4319b9d4458b5ec94d8f22a4df01f7a22d09d384
Thomas McNish <mcnishthomas@gmail.com>
2022-05-20 08:17:10 -0400

    added buttons for decreasing and resetting score

--- src/App.js
+++ src/App.js
@@ -3,12 +3,16 @@ import './App.css';
 
 function App() {
   const [score, setScore] = useState(0); // [0, f]
+  const [message, setMessage] = useState('Welcome!');
 
   return (
     <div className="App">
       <header className="App-header">
-        <h1>{score}</h1>
-        <button onClick={() => setScore(score + 1)} >Increase Score </button>
+        <h1>{message}</h1>
+        <h2>{score}</h2>
+        <button onClick={() => setScore(prevScore => prevScore + 1)} >Increase Score </button>
+        <button onClick={() => setScore(score - 1)} >Decrease Score </button>
+        <button onClick={() => setScore(0)}>Reset Score</button>
       </header>
     </div>
   );
